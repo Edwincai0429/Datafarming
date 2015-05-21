f9197573ebd5bda2f108a560bb9eaeb189c28d0c
pjs <pjs@alum.mit.edu>
2015-05-21 12:57:01 -0700

    cleanup of mser-b

--- mser-b.rb
+++ mser-b.rb
@@ -1,68 +1,75 @@
 #!/usr/bin/env ruby -w
 
-if RUBY_VERSION =~ /^1\.8/
-  require 'rubygems'
+require 'rubygems' if RUBY_VERSION =~ /^1\.8/
+
+class String
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def yellow
+    colorize(33)
+  end
 end
 
-# You must have the quickstats RubyGem installed.  Assuming
-# you have network connectivity, type:
-#
-#     gem install quickstats
-#
-# Note that this may require admin privileges.
-require 'quickstats'
-# require 'weightedstats'
+begin
+  require 'quickstats'
+rescue LoadError
+  STDERR.puts "\n\tALERT: quickstats gem is not installed!".red
+  STDERR.puts "\tIf you have network connectivity, type:"
+  STDERR.puts "\n\t\tgem install quickstats\n".yellow
+  STDERR.puts "\t(Admin privileges may be required.)\n\n"
+  exit(-1)
+end
 
 def readbatch(data, index, b)
-  (data[(index+1-b)..index].inject {|x,sum| sum+=x}) / b  
+  (data[(index + 1 - b)..index].inject { |x, sum| sum += x }) / b
 end
 
 if ARGV.length > 0
   batch_size = 1
-  if ARGV[0].to_i > 0
-    batch_size = ARGV.shift.to_i
-  end
 
-  printf "x-bar,n\n"
+  batch_size = ARGV.shift.to_i if ARGV[0].to_i > 0
 
-  # ws = WeightedStats.new
+  puts 'x-bar,n'
 
   ARGV.each do |fname|
     data = File.readlines(fname)
     data.shift if data[0] =~ /[A-Za-z]/
-    data.map! {|line| line.chomp.strip.to_f}
+    data.map! { |line| line.chomp.strip.to_f }
     mser_stats = QuickStats.new
     warmup = [(data.length * 0.5).to_i, data.length - 10].min
     index = data.length - 1
-    while index > (data.length - warmup)  && index > batch_size do
+    while index > (data.length - warmup) && index > batch_size
       mser_stats.new_obs(readbatch(data, index, batch_size))
       index -= batch_size
     end
     best = [mser_stats.std_err, mser_stats.avg, warmup]
 
-    while index >= batch_size do
+    while index >= batch_size - 1
       mser_stats.new_obs(readbatch(data, index, batch_size))
-      index -= batch_size
       if mser_stats.std_err < best[0]
         best = [mser_stats.std_err, mser_stats.avg, index]
       end
+      index -= batch_size
     end
 
     printf "%f,%d\n", best[1], data.length - best[2]
-    # ws.new_obs(best[1], data.length - best[2])
-
   end
-  # printf "avg = %f, std err = %f, df = %f\n", ws.avg, ws.std_err, ws.df
 else
   STDERR.puts
-  STDERR.puts " Must supply at least one argument.  Syntax:"
+  STDERR.puts ' Must supply at least one argument.  Syntax:'
   STDERR.puts
-  STDERR.puts "\truby #{$0} [batchsize] filename(s)..."
+  STDERR.puts "\truby #{$PROGRAM_NAME} [batchsize] filename(s)...".yellow
   STDERR.puts
-  STDERR.puts " The batchsize is optional and defaults to 1 if not provided."
-  STDERR.puts " The files specified as filenames (explicit list or wildcard)"
-  STDERR.puts " should be one column of output data per file, with no headers."
-  STDERR.puts " Results are written to stdout and can be redirected as desired."
+  STDERR.puts ' The batchsize is optional and defaults to 1 if not provided.'
+  STDERR.puts ' The files specified as filenames (explicit list or wildcard)'
+  STDERR.puts ' should be one column of output data per file, with no headers.'
+  STDERR.puts ' Results are written to stdout and can be redirected as desired.'
   STDERR.puts
 end
-
