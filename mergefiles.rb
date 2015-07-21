3a00050876dce521713f3390d7db1b1cfc503ac9
pjs <pjs@alum.mit.edu>
2015-07-21 15:58:04 -0700

    cleaned mergefiles a'la rubocop

--- mergefiles.rb
+++ mergefiles.rb
@@ -6,26 +6,26 @@
 # source of the data to be merged.  Subsequent lines are the
 # contents of that set of files.
 #
-# If "-n" or "--no-labels" is provided as a command-line argument,
+# If '-n' or '--no-labels' is provided as a command-line argument,
 # then labels within the individual files are NOT copied through.
 #
 # Output is sent to STDOUT, where it can be redirected as desired.
 
 # First, process any command-line arguments
 no_labels = false
-while ARGV[0] && (ARGV[0][0] == "-" || ARGV[0][0] == 45)
+while ARGV[0] && (ARGV[0][0] == '-' || ARGV[0][0] == 45)
   case ARGV.shift
-  when "--no-labels", "-n"
+  when '--no-labels', '-n'
     no_labels = true
   else
-    STDERR.puts "Unknown argument!"
+    STDERR.puts 'Unknown argument!'
   end
 end
 
 old_filename = nil
 line_set = nil
 allfiles = []
-labels = ARGV.join(",")
+labels = ARGV.join(',')
 
 # Read in all data from all files, resetting the line_set
 # for each new file
@@ -42,9 +42,9 @@ end
 allfiles << line_set
 
 # Equalize all vectors to same length by padding with nils if needed
-max_length = (allfiles.map {|v| v.length}).max
-allfiles.each {|v| v[max_length - 1] = nil unless v.length == max_length}
+max_length = allfiles.map(&:length).max
+allfiles.each { |v| v[max_length - 1] = nil unless v.length == max_length }
 # Construct and print the filename labels...
 puts labels
 # ...and output all the data
-allfiles.transpose.each {|row| puts row.join(",")}
+allfiles.transpose.each { |row| puts row.join(',') }
