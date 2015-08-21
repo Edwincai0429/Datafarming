b5e2b0adf763453212b9e632a9e44a5242a88804
pjs <pjs@alum.mit.edu>
2015-08-21 15:39:17 -0700

    significant tightening of code in cross.rb

--- cross.rb
+++ cross.rb
@@ -2,38 +2,27 @@
 
 # The "cross" method creates a large combinatorial
 # design by crossing all combinations of individual
-# smaller designs.  It uses a recursive back-end to
-# do so because we don't know how many designs there
+# smaller designs.  It uses recursion to do so
+# because we don't know how many designs there
 # may be in the input set.
 #
 # The method takes an array of arrays, where
 # each sub-array contains a single component design,
 # and kicks off the recursive build process.
-def cross(array_of_arrays)
-  solution = []
-  recursive_build(array_of_arrays, 0, [], solution)
-  solution
-end
-
-private
-
-# The following should never be invoked by end users, it is
-# only called by the cross method after suitable setup
-def recursive_build(inputs, index, partial_soln, full_soln)
-  if index >= inputs.size
-    full_soln << partial_soln.join(',')
+def cross(inputs, idx = 0, tmp = [], solution = [])
+  if idx >= inputs.size
+    solution << tmp.join(',')
   else
-    inputs[index].each do |line|
-      recursive_build(inputs, index + 1, partial_soln + line, full_soln)
-    end
+    inputs[idx].each { |dp| cross(inputs, idx + 1, tmp + dp, solution) }
   end
+  solution
 end
 
 # The remainder is effectively the "main" for this script
 if __FILE__ == $PROGRAM_NAME
   if ARGV.length > 1
     input_array = []
-    ARGV.each do |filename|     # for each file given as a command-line arg...
+    ARGV.each do |filename|  # for each file given as a command-line arg...
       # open the file, read all the lines, and then for each line use
       # spaces, commas, colons, or semicolons to tokenize.
       input_array << File.open(filename).readlines.map do |line|
