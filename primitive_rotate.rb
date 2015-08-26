4bfd2cabd64dd67dc5a1dba76fc4f4a453a4cdb5
pjs <pjs@alum.mit.edu>
2015-08-25 17:27:24 -0700

    Build in designs as design_table

--- primitive_rotate.rb
+++ primitive_rotate.rb
@@ -1,45 +1,44 @@
 #!/usr/bin/env ruby -w
 
+require_relative './design_table.rb'
+
+# Scaler objects will rescale a Latin Hypercube design from standard units
+# to a range as specified by min, max, and num_decimals
 class Scaler
   def initialize(min, max, num_decimals, lh_max = 17)
     @min = min
-    @range = max - min
-    @lh_max = (lh_max - 1).to_r
-    @scale_factor = 10**num_decimals
+    @range = (max - min) / (lh_max - 1).to_r
+    @scale_factor = 10r**num_decimals
   end
 
   def scale(value)
-    new_range = @range * (value.to_r - 1) / @lh_max
+    new_value = @min + @range * (value.to_r - 1r)
     if @scale_factor == 1
-      @min + new_range.round
+      new_value.round
     else
-      @min + (@scale_factor * new_range).round / @scale_factor.to_f
+      ((@scale_factor * new_value).round / @scale_factor).to_f
     end
   end
 end
 
-design = []
-lh_size = 0
-while line = STDIN.gets
-  design << line.strip.split(/[,;:]|\s+/).map!(&:to_i)
-  lh_size += 1
-end
+lh_size = 17
 
 scale_obj = [
   Scaler.new(-8, 8, 0, lh_size),
   Scaler.new(-16, 16, 0, lh_size),
   Scaler.new(-8, 8, 1, lh_size),
   Scaler.new(-8, 8, 2, lh_size),
-  Scaler.new(-8, 8, 3, lh_size),
-  Scaler.new(-1, 1, 0, lh_size),
-  Scaler.new(-1, 1, 6, lh_size)
+  Scaler.new(1, 17, 0, lh_size),
+  Scaler.new(0, 1, 0, lh_size),
+  Scaler.new(0, 10, 2, lh_size)
 ]
 
+design = DESIGN_TABLE[lh_size]
 num_rotations = (ARGV.shift || design[0].length).to_i
 num_rotations.times do
   design.each_with_index do |dp, i|
     scaled_dp = dp.map.with_index { |x, k| scale_obj[k].scale(x) }
-    puts scaled_dp.join ','
+    puts scaled_dp.join "\t"
     design[i] = dp.rotate
   end
 end
