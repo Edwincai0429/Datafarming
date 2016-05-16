b7e1dc5c1b1741815caebcdca8b0a37dec5b14ff
pjs <pjs@alum.mit.edu>
2016-05-15 17:54:21 -0700

    refactored rundesign_general.rb

--- rundesign_general.rb
+++ rundesign_general.rb
@@ -1,20 +1,61 @@
 #!/usr/bin/env ruby -w
 
-print = false # default is to run rather than print
+require 'colorize'
+
+String.disable_colorization false
+
+require 'optparse'
+require_relative 'error_handling'
+
+help_msg = [
+  'Run control to apply a designed experiment to a model with replication.', '',
+  'This script assumes that the model uses command-line arguments to',
+  'set factor values at run-time.', '',
+  'Syntax:',
+  "\n    ruby #{ErrorHandling.prog_name} [OPTIONS] ".yellow +
+    "'CMD' DOE_FILE #REPS OUTPUT_FILE".yellow, '',
+  "A vertical bar '|' indicates valid alternatives for OPTIONS.",
+  'Valid OPTIONS are:', '',
+  '  --help | -h | -? | ?'.green,
+  "\tProduce this help message.",
+  '  --print | -p'.green,
+  "\tPrint generated commands rather than executing them,",
+  "\tuseful for debugging.",
+  '  --destructive | -d'.green,
+  "\tOverwrite any prior contents in the output file.  Default",
+  "\tbehavior is to append new results to an existing output file.", '',
+  'Required arguments are:', '',
+  "  'CMD'".green,
+  "\tThe command to run the model.  " +
+    'MUST be placed in single quotes'.red,
+  "\tif the command contains any white space or special characters.",
+  "\tExample: " + "'java MyModel.jar'".blue,
+  '  DOE_FILE'.green,
+  "\tThe name of a text file containing the experimental design",
+  "\tto be used.  The design file should have one line per design",
+  "\tpoint with factor settings separated by white space.  Factor",
+  "\tsettings must be provided in the order expected by the model.",
+  '  #REPS'.green,
+  "\tAn integer specifying the number of times each design point",
+  "\tshould be replicated.  All design points are completed before",
+  "\tmoving to the next replication to minimize the risk of missing",
+  "\tdesign points if the run gets interrupted for any reason.",
+  '  OUTPUT_FILE'.green,
+  "\tThe name of a text file to which all output will be written."
+]
+
+print_cmds = false # default is to run rather than print
 destructive = false # default is non-destructive for output file
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$PROGRAM_NAME} [-h|--help] [filenames...[]"
+  opts.on('-h', '-?', '--help') { ErrorHandling.clean_abort help_msg }
+  opts.on('-p', '--print') { print_cmds = true }
+  opts.on('-d', '--destructive') { destructive = true }
+end.parse!
 
-while ARGV[0] && (ARGV[0][0] == '-' || ARGV[0][0] == 45)
-  case ARGV.shift
-  when '--print', '-p'
-    print = true
-  when '--destructive', '-d'
-    destructive = true
-  else
-    STDERR.puts 'Unknown argument!'
-  end
-end
+ErrorHandling.clean_abort help_msg if ARGV[0] == '?' || ARGV.length != 4
 
-if ARGV.length == 4
+begin
   # What shall we run today?
   cmd = ARGV.shift
   # Suck in all the design points from the file specified as the next
@@ -25,11 +66,10 @@ if ARGV.length == 4
   # Where do the results go?
   output_file_name = ARGV.shift
   File.delete(output_file_name) if destructive && File.exist?(output_file_name)
-
   reps.times do
     design_pts.each do |design_pt|
       exe_line = "#{cmd} #{design_pt} >> #{output_file_name}"
-      if print
+      if print_cmds
         puts exe_line
       else
         result = `#{exe_line}`
@@ -37,15 +77,7 @@ if ARGV.length == 4
       end
     end
   end
-else
-  STDERR.puts "\n  This script requires 4 command line args:"
-  STDERR.puts "\tThe command to be run (MUST be in single quotes)"
-  STDERR.puts "\tThe name of the DOE file (scaled, no headers or extra columns)"
-  STDERR.puts "\tThe number of times to replicate each design point"
-  STDERR.puts "\tThe name of the output file to which results will be sent\n"
-  STDERR.puts "\n  Note: run-time options, if present, must be specified"
-  STDERR.puts '  before the 4 required command-line arguments.'
-  STDERR.puts "\n  Options:"
-  STDERR.puts "\n\t--print or -p: print to STDOUT rather than executing"
-  STDERR.puts "\n\t--destructive or -d: delete pre-existing output file\n\n"
+rescue StandardError => e
+  ErrorHandling.message [e.message.red]
+  ErrorHandling.clean_abort help_msg
 end
